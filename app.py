5f3be8b79e55cf02fd567e49c3d580901463414a
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-11-11 20:58:47 -0800

    renamed app and added docstring

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+"""API for data at http://www.theatlantic.com/politics/archive/2016/11/where-republicans-stand-on-donald-trump-a-cheat-sheet/481449/"""
 from bs4 import BeautifulSoup
 from tinydb import TinyDB, Query
 import requests
@@ -10,8 +10,10 @@ def get_html() -> str:
     """
     Return the text of the html document.
     """
-    corpus = requests.get("http://www.theatlantic.com/politics/archive/2016/11/where-republicans-stand-on-donald-trump-a-cheat-sheet/481449/").text
+    corpus = requests.get(
+        "http://www.theatlantic.com/politics/archive/2016/11/where-republicans-stand-on-donald-trump-a-cheat-sheet/481449/").text
     return corpus
+
 
 def get_data_from_html(html: str) -> [dict]:
     """
@@ -41,20 +43,19 @@ def get_data_from_html(html: str) -> [dict]:
 
     return result
 
+
 @hug.get('/republicans', examples="position=nay")
-def name(name: hug.types.text='', position: hug.types.text=''):
+def name(name: hug.types.text = '', position: hug.types.text = ''):
     db = TinyDB('republicans.json')
     Republican = Query()
 
     name_re = re.compile(name, re.I)
     position_re = re.compile(position, re.I)
 
-
     if name and position:
         return ["you must use either name or position"]
     elif name:
         result = db.search(Republican.name.search(name_re))
-
     elif position:
         result = db.search(Republican.position.search(position_re))
     else:
@@ -64,11 +65,6 @@ def name(name: hug.types.text='', position: hug.types.text=''):
         "count": len(result),
         "result": result,
     }
-
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -83,4 +79,3 @@ if __name__ == '__main__':
         db.insert(r)
 
     pprint(db.all())
-
